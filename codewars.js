3c61988ce59180ea0e253da6b84a3bd2c782cb74
Amber Oglesby <aoglesby5795@gmail.com>
2023-01-10 07:24:58 -0500

    #2,3

--- codewars.js
+++ codewars.js
@@ -15,14 +15,34 @@ newArr.1
 // Filling an array (part 1)
 // https://www.codewars.com/kata/571d42206414b103dc0006a1/train/javascript
 
+//Array.from() static method creates a new, shallow-copied Array instance from an iterable or array-like object.
+//index:The index of the current element being processed in the array.
+//Array.from(arrayLike, (element, index) => { /* … */ })
 
+const arr = N => Array.from{length:N},(_,index)=>index
 
+/* Array.from() lets you create Arrays from:
+
+-iterable objects (objects such as Map and Set); or, if the object is not iterable,
+-array-like objects (objects with a length property and indexed elements).
+*/
 
 // # 3
 // Drone Fly-By
 // https://www.codewars.com/kata/58356a94f8358058f30004b5/train/javascript
 
+// I found an answer but dont quite understand it and plan to ask for clarification during lab 
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
 
+function flyBy(lamps, drone) {
+    var lampArr = lamps.split('');
+    for (let i=0; i < drone.length; i++) {
+      if (lampArr[i] === "x") {
+        lampArr.splice(i, 1, "o");
+      }
+    }
+    return lampArr.join('');
+  }
 
 
 // # 4
